81c0ca4ccc94c73b556b5aec4872bb7cc4b45e8c
Yang <651033755@qq.com>
2020-09-10 10:35:16 +0800

    添加画笔和橡皮功能、active样式

--- canvas2/main.js
+++ canvas2/main.js
@@ -8,47 +8,82 @@ var lastPoint = { x: undefined, y: undefined }
 
 setCanvasSize()
 autoSetCanvasSize()
-
+listenToUser(ctx)
 
 
 
 //Listen to user
-eraser.onclick = function () {
-    eraserEnable = !eraserEnable
-}
-ctx.onmousedown = function (a) {
-    var x = a.clientX
-    var y = a.clientY
-    using = true
-    if (eraserEnable) {
-        context.clearRect(x-5, y-5, 10, 10)
-    } else {
-        lastPoint = { "x": x, "y": y }
-        drawCircle(x, y, 1)
+function listenToUser(ctx){
+    
+    
+    if(document.body.ontouchstart !== undefined){
+        //Mobile
+        ctx.ontouchstart = function(a){
+            var x = a.touches[0].clientX
+            var y = a.touches[0].clientY
+            using = true
+            if(eraserEnable){
+                context.clearRect(x-5, y-5, 10, 10)
+            }else {
+                lastPoint = { "x": x, "y": y }
+                drawCircle(x, y, 1)
+        
+            }
+        }
+        ctx.ontouchmove = function(a){
+            var x = a.touches[0].clientX
+            var y = a.touches[0].clientY
+            if(eraserEnable){
+                if (using) {
+                    context.clearRect(x - 5, y - 5, 10, 10)
+                }
+            }else {
+                if (using) {
+                    var newPoint = { "x": x, "y": y }
+                    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                    drawCircle(x, y, 5)
+                    lastPoint = newPoint
+                }
+            }
+        }
+        ctx.ontouchend = function(){
+            using = false
+        }
+    }else{
+        //PC
+        ctx.onmousedown = function (a) {
+            var x = a.clientX
+            var y = a.clientY
+            using = true
+            if (eraserEnable) {
+                context.clearRect(x-5, y-5, 10, 10)
+            } else {
+                lastPoint = { "x": x, "y": y }
+                drawCircle(x, y, 1)
+            }
+        }
+        ctx.onmousemove = function (a) {
+            var x = a.clientX
+            var y = a.clientY
+            if(!using){return}
+            if (eraserEnable) {   
+                    context.clearRect(x-5, y-5, 10, 10) 
+            } else {
+                    var newPoint = { "x": x, "y": y }
+                    drawCircle(x, y, 1)
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint 
+            }
+        }
+        ctx.onmouseup = function (a) {
+            using = false      
+        }
 
     }
-
-
-
 }
-ctx.onmousemove = function (a) {
-    var x = a.clientX
-    var y = a.clientY
-    if(!using){return}
-    if (eraserEnable) {   
-            context.clearRect(x-5, y-5, 10, 10) 
-    } else {
-            var newPoint = { "x": x, "y": y }
-            drawCircle(x, y, 1)
-        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-        lastPoint = newPoint 
-    }
 
-}
-ctx.onmouseup = function (a) {
-    using = false
 
-}
+
 function drawCircle(x, y, radius) {
     context.beginPath()
     context.arc(x, y, radius, 0, Math.PI * 2)
@@ -79,3 +114,30 @@ function setCanvasSize() {
     canvas.width = pageWidth
     canvas.height = pageHeight
 }
+
+//tool
+eraser.onclick = function () {
+    eraserEnable = true
+    eraser.classList.add('active')
+    pen.classList.remove('active')
+}
+pen.onclick = function(){
+    eraserEnable = false
+    pen.classList.add('active')
+    eraser.classList.remove('active')
+} 
+clear.onclick = function(){
+    context.clearRect(0,0,canvas.width,canvas.height)
+} 
+download.onclick = function(){
+    var url = canvas.toDataURL("image/png")
+    var a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href=url
+    a.download = 'image'
+    a.target = '_blank'
+    a.click()
+}
+sizes.onclick = function(){
+    sizes.classList.add('active')
+} 
